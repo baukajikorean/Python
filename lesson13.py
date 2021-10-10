5a318c369f746628fd10c2012770cb11f6ba3408
baukajikorean <dzetso@gmail.com>
2021-10-10 18:04:52 +0500

    first commit

--- lesson13.py
+++ lesson13.py
@@ -14,12 +14,31 @@
 
 
 
-# FOR
+# for
 
-s = "Hello world"
-for l in s:
-    if l != " ":
-        print(l, end="-")
-    else:
-        print("-", end="-")
+# continue
+# s = "Hello world"
+# for l in s:
+#     if l == " ":
+#         continue
+#     else:
+#         print(f'"{l}"', end=" ")
 
+# break
+
+# for i in "Helloworld":
+#     if i == " ":
+#         break
+#     print(i, end=" ")
+# else:
+#     print("\nCycle finished successfully.")
+
+# 1900-2021
+
+# for i in range(1900, 2022):
+#     print(i, end=" ")
+
+# i = 1900
+# while i <= 2021:
+#     print(i, end=" ")
+#     i += 1
