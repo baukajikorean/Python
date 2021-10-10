0a79712fdbb1603470a380997c81ff735a5e43fc
baukajikorean <bawka9696@gmail.com>
2021-10-10 16:46:31 +0500

    First Commit

---
+++ lesson13.py
@@ -0,0 +1,25 @@
+# WHILE
+
+# while True:
+#     print("Hello")
+
+# i = 1
+# while i <= 10:
+#     print(f"Your number is {i}{' .' if i < 10 else '.'}")
+#     # i = i + 1
+#     i += 1
+
+# print("Hello", "world", end=" ")
+# print("Hello2", "world2", end=" ")
+
+
+
+# FOR
+
+s = "Hello world"
+for l in s:
+    if l != " ":
+        print(l, end="-")
+    else:
+        print("-", end="-")
+
